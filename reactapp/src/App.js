b83a5a3746aea0400e0a8825947e48780eb2e6c6
jayasree19 <jonnadulajayasree@gmail.com>
2023-07-14 05:19:12 +0000

    Revert "Revert " initial commit""
    This reverts commit 725dcb340c75ccf04283af63fdfbe06de4ace0e8.

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup"
 import "./components/Signup/Signup.css"
@@ -41,6 +42,29 @@ function App() {
 
       </Routes>
     </BrowserRouter>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> parent of 725dcb3 (Revert " initial commit")
   );
 }
 
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
+=======
+import ReactDOM from 'react-dom';
+>>>>>>> parent of 725dcb3 (Revert " initial commit")
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<<<<<<< HEAD
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+=======
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+>>>>>>> parent of 725dcb3 (Revert " initial commit")
 );
 
 // If you want to start measuring performance in your app, pass a function
